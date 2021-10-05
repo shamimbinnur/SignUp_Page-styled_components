7e31e74b914f8d9e1617fec3dac6b32893fd2632
Shamim Bin Nur <48399373+shamimbinnur@users.noreply.github.com>
2021-10-05 23:29:32 +0600

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,3 +1,10 @@
+// 	AUTHOR:		SHAMIM BIN NUR
+// 	GITHUB:		https://github.com/shamimbinnur
+// 	LINKEDIN:	https://www.linkedin.com/in/shamimbinnur
+// 	MAIL:		iamshamimbn@gmail.com
+// 	SITE:		www.shamimbinnur.me
+
+
 import React from "react";
 import styled from "styled-components";
 import bgImg from "./assets/57.jpg";
